855d01039b6d100f7ba2771592d14af312cae8ce
Maarten Hogeveen <maarten@Maartens-MacBook-Air.local>
2020-07-28 15:18:08 +0200

    Remove unnecessary react portal

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 
 const sections = [
    {
@@ -21,15 +20,14 @@ const sections = [
 ]
 
 const Drawer = ({ navOut }) => {
-   return ReactDOM.createPortal(
+   return (
       <div className={navOut ? 'drawer active' : 'drawer'}>
          {sections.map((section) => (
             <a key={section.id} className='drawer-item' href={`#${section.id}`}>
                <span className='drawer-item_title'>{section.name}</span>
             </a>
          ))}
-      </div>,
-      document.querySelector('#modal')
+      </div>
    )
 }
 
