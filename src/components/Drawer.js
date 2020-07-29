08ff219fee6ab7e3eaa6cef7604f054b8612866c
Maarten Hogeveen <maarten@Maartens-MacBook-Air.local>
2020-07-29 14:25:32 +0200

    Reconfigure navigation

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -22,6 +22,7 @@ const sections = [
 const Drawer = ({ navOut, handleNav }) => {
    return (
       <nav className={navOut ? 'drawer active' : 'drawer'}>
+         <div className='drawer-background'></div>
          {sections.map((section) => (
             <a key={section.id} className='drawer-item' href={`#${section.id}`} onClick={handleNav}>
                <span className='drawer-item-title'>{section.name}</span>
