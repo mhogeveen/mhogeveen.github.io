ec53ac81378df9fed2d295a4ad40b32e1ba8e302
Maarten Hogeveen <maarten@Maartens-MacBook-Air.local>
2020-08-17 14:32:10 +0200

    Add project and fix styling for projects

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -1,8 +1,16 @@
+import github_repo_timeline from './github_repo_timeline.webp'
 import dnd_dice_roller from './dnd_dice_roller.webp'
 import checklist from './checklist.webp'
 import pure from './pure.webp'
 
 export const projectsArray = [
+   {
+      name: 'Github Repo Timeline',
+      website: 'https://mhogeveen.github.io/github_repo_timeline/',
+      code: 'https://github.com/mhogeveen/github_repo_timeline',
+      image: github_repo_timeline,
+      icons: ['react', 'redux', 'javascript', 'sass'],
+   },
    {
       name: 'DnD Dice Roller',
       website: 'https://mhogeveen.github.io/dnd_dice_roller/',
