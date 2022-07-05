1b0c884385d78b115e9f91a2bb0dc1273c830589
Maarten Hogeveen <maarten@Maartens-MacBook-Pro.local>
2022-07-05 08:57:02 +0200

    Updates

---
+++ precache-manifest.a2b104bca2d65dfa9148c5c28f4de635.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c970412a1b854701bcf16ddf12aeb377",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9971f54d32297b7641df",
+    "url": "/static/css/main.9bea69fd.chunk.css"
+  },
+  {
+    "revision": "df39ad91e72e0563493c",
+    "url": "/static/js/2.8bc59376.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/static/js/2.8bc59376.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9971f54d32297b7641df",
+    "url": "/static/js/main.2ca5e212.chunk.js"
+  },
+  {
+    "revision": "18214172caed885effdd",
+    "url": "/static/js/runtime-main.bccdc93b.js"
+  },
+  {
+    "revision": "efea0e5661c9e95da5a79feee050ee24",
+    "url": "/static/media/checklist.efea0e56.webp"
+  },
+  {
+    "revision": "f58c21d22ed70e478b83ea75ad2586f5",
+    "url": "/static/media/dnd_dice_roller.f58c21d2.webp"
+  },
+  {
+    "revision": "aa30e3b3bf2aae78ce6e184b7020dffe",
+    "url": "/static/media/github_repo_timeline.aa30e3b3.webp"
+  },
+  {
+    "revision": "c1f51e90f8cce489dc642e4f87a039c4",
+    "url": "/static/media/profile.c1f51e90.webp"
+  },
+  {
+    "revision": "76a77400d77f897b7ab3e161cbb442dc",
+    "url": "/static/media/pure.76a77400.webp"
+  }
+]);
