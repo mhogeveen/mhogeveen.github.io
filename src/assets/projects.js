4b197ec637b290a08ec956ce3e838784cf092abd
Maarten Hogeveen <maarten@maartens-mbp.lan>
2022-07-06 09:09:42 +0200

    Change around projects

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -1,9 +1,16 @@
+import tic_tac_toe from './tic_tac_toe.webp'
 import github_repo_timeline from './github_repo_timeline.webp'
 import dnd_dice_roller from './dnd_dice_roller.webp'
 import checklist from './checklist.webp'
-import pure from './pure.webp'
 
 export const projectsArray = [
+     {
+      name: 'Tic-Tac-Toe',
+      website: 'https://mhogeveen.github.io/tic-tac-toe',
+      code: 'https://github.com/mhogeveen/tic-tac-toe',
+      image: tic_tac_toe,
+      icons: ['react', 'redux', 'typescript', 'sass'],
+   },
    {
       name: 'Github Repo Timeline',
       website: 'https://mhogeveen.github.io/github_repo_timeline/',
@@ -24,12 +31,5 @@ export const projectsArray = [
       code: 'https://github.com/mhogeveen/JavaScript_ToDoList',
       image: checklist,
       icons: ['javascript', 'sass', 'html'],
-   },
-   {
-      name: 'Pure',
-      website: 'https://foundryremake.firebaseapp.com/',
-      code: 'https://github.com/mhogeveen/FoundryRemake',
-      image: pure,
-      icons: ['jquery', 'sass', 'html'],
    },
 ]
